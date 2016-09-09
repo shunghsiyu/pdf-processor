81b5a5d597572ea330fc11f30f56412c32af8555
shunghsiyu <shunghsiyu@gmail.com>
2016-09-09 23:03:58 +0800

    Add comments for `main()`

--- Main.py
+++ Main.py
@@ -19,22 +19,32 @@ parser.add_argument(
 
 
 def main():
+    # Get to directory with PDF files to work on
     args = parser.parse_args()
     directory = args.directory
+
+    # Open the PDF files
     all_pdf_files = [os.path.join(directory, filename) for filename in all_pdf_files_in_directory(directory)]
     opened_files = map(lambda path: open(path, 'rb'), all_pdf_files)
+
     all_pages = concat_pdf_pages(opened_files)
 
+    # For all pages that belongs to the same document ID
     for idx, pages in enumerate(split_on_condition(all_pages, predicate=width_greater_than_height), start=1):
+        # Put those pages into a writer
         pdf_writer = PdfFileWriter()
         map(pdf_writer.addPage, pages)
 
+        # And write those pages to a single PDF file
         output_filename = '{0:05}.pdf'.format(idx)
         with open(output_filename, 'wb') as output_file:
             pdf_writer.write(output_file)
+
+            # Extra measures to make sure data is written to disk
             output_file.flush()
             os.fsync(output_file.fileno())
 
+    # Make sure to close all the files that were opened
     map(lambda f: f.close, opened_files)
 
 
