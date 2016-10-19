0b8cb5b18a7d5336432b9af87cee123da32428a0
shunghsiyu <shunghsiyu@gmail.com>
2016-10-20 01:44:20 +0800

    Update GUI.py
    * Allow configuration of rotation correction.

--- GUI.py
+++ GUI.py
@@ -2,9 +2,8 @@
 
 import Tkinter as tk
 import logging
-import tkFileDialog, tkMessageBox
-
-import Main
+import tkFileDialog
+import tkMessageBox
 
 log = logging.getLogger(__name__)
 
@@ -13,7 +12,7 @@ store = {}
 
 def gui_setup():
     def run():
-        main_config = ['-r', 'cw', '-d', '-e', '-m', '-f', str(threshold_scale.get() / 100), '-v',
+        main_config = ['-r', rotation_var.get(), '-d', '-e', '-m', '-f', str(threshold_scale.get() / 100), '-v',
                        store.get('input', '')]
         log.info('Process PDF with config %s', repr(main_config))
         Main.main(main_config)
@@ -27,21 +26,31 @@ def gui_setup():
 
     root = tk.Tk()
 
-    frame1 = tk.Frame(root)
-    frame1.pack(side=tk.TOP)
-    blank_page_threshold_label = tk.Label(frame1, text=u'空白頁偵測')
+    frame_rotation = tk.Frame(root)
+    frame_rotation.pack(side=tk.TOP)
+    rotation_label = tk.Label(frame_rotation, text=u'旋轉方向')
+    rotation_label.pack(side=tk.LEFT)
+    rotation_var = tk.StringVar()
+    rotation_var.set('cw')
+    tk.Radiobutton(frame_rotation, text=u'順時針', variable=rotation_var, value='cw').pack(side=tk.LEFT)
+    tk.Radiobutton(frame_rotation, text=u'逆時針', variable=rotation_var, value='ccw').pack(side=tk.LEFT)
+    tk.Radiobutton(frame_rotation, text=u'不旋轉', variable=rotation_var, value='no-op').pack(side=tk.LEFT)
+
+    frame_threshold = tk.Frame(root)
+    frame_threshold.pack(side=tk.TOP)
+    blank_page_threshold_label = tk.Label(frame_threshold, text=u'空白頁偵測')
     blank_page_threshold_label.pack(side=tk.LEFT)
-    threshold_scale = tk.Scale(frame1, orient=tk.HORIZONTAL, resolution=0.1, sliderlength=15, length=600)
+    threshold_scale = tk.Scale(frame_threshold, orient=tk.HORIZONTAL, resolution=0.1, sliderlength=15, length=600)
     threshold_scale.set(2)
     threshold_scale.pack(side=tk.LEFT)
 
-    frame2 = tk.Frame(root)
-    frame2.pack(side=tk.TOP)
-    input_label = tk.Label(frame2, text=u'輸入資料夾')
+    frame_input = tk.Frame(root)
+    frame_input.pack(side=tk.TOP)
+    input_label = tk.Label(frame_input, text=u'輸入資料夾')
     input_label.pack(side=tk.LEFT)
-    input_dir_label = tk.Label(frame2, wraplength=600)
+    input_dir_label = tk.Label(frame_input, wraplength=600)
     input_dir_label.pack(side=tk.TOP)
-    choose_input_button = tk.Button(frame2, text=u'選擇輸入資料夾', command=choose_input)
+    choose_input_button = tk.Button(frame_input, text=u'選擇輸入資料夾', command=choose_input)
     choose_input_button.pack(side=tk.TOP)
 
     start_button = tk.Button(root, text=u'開始', command=run)
